ef2db17c3cfd4c31d2b6102abe0a134e96e62bb0
Jheymis <rodriguesjheymis@gmail>
2023-04-11 10:38:00 -0300

    Projeto Cronometro

--- cronometro-js/JS/scripts.js
+++ cronometro-js/JS/scripts.js
@@ -1,29 +1,32 @@
-const minutesEl = document.querySelector("#minutes")
-const secondsEl = document.querySelector("#seconds")
-const milisecondsEl = document.querySelector("#miliseconds")
-const startBtn = document.querySelector("#startBtn")
-const pauseBtn = document.querySelector("#pauseBtn")
-const resumeBtn = document.querySelector("#resumeBtn")
-const resetBtn = document.querySelector("#resetBtn")
+const minutesEl = document.querySelector("#minutes");
+const secondsEl = document.querySelector("#seconds");
+const millisecondsEl = document.querySelector("#milliseconds");
+const startBtn = document.querySelector("#startBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+const resumeBtn = document.querySelector("#resumeBtn");
+const resetBtn = document.querySelector("#resetBtn");
 
 let interval;
 let minutes = 0;
 let seconds = 0;
-let miliseconds = 0;
+let milliseconds = 0;
 let isPaused = false;
 
-startBtn.addEventListener("click", startTimer)
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click",pauseTimer);
+resumeBtn.addEventListener("click",resumeTimer);
+resetBtn.addEventListener("click",resetTimer);
 
 function startTimer() {
 
 interval = setInterval (() => {
 
-    if (!isPaused){
-        miliseconds += 10
+    if (!isPaused) {
+        milliseconds += 10;
 
-        if (miliseconds === 1000) {
+        if (milliseconds === 1000) {
             seconds++;
-            miliseconds = 0;
+            milliseconds = 0;
         }
         if (seconds ===60) {
             minutes++;
@@ -31,15 +34,46 @@ interval = setInterval (() => {
         }
         minutesEl.textContent = formatTimer(minutes);
         secondsEl.textContent = formatTimer(seconds);
-        milisecondsEl.textContent = formatMilliseconds(miliseconds);
+        millisecondsEl.textContent = formatmilliseconds(milliseconds);
     }
-},10)
+},10);
+
+startBtn.style.display = "none";
+pauseBtn.style.display = "block";
 }
+
+function pauseTimer (){
+    isPaused = true;
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "block";
+}
+
+function resumeTimer (){
+    isPaused = false;
+    pauseBtn.style.display = "block";
+    resumeBtn.style.display = "none";
+}
+
+
+ function resetTimer(){
+    clearInterval(interval);
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+
+    minutesEl.textContent = "00";
+    secondsEl.textContent = "00";
+    millisecondsEl.textContent = "000";
+
+    startBtn.style.display = "block";
+    pauseBtn.style.display = "none";
+    resumeBtn.style.display = "none";
+ }
 
 function formatTimer(time) {
     return time < 10 ? `0${time}` : time;
 }
 
-function formatMilliseconds(time) {
-    return time < 100 ? `${time}`.padStart (3,"0") : time;
+function formatmilliseconds(time) {
+    return time < 100 ? `${time}`.padStart(3,"0") : time;
 }
